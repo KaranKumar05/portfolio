916f6e6d19605ba00260a6423ff1e412010bb94d
KARAN KUMAR <kkvmalhi@gmail.com>
2023-03-18 01:50:13 +0500

    working on js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
-let typed = new typed(".multi-headline",{
-    strings:["Frontend Developer",  "MERN Developer","Graphic Designer","Freelancer","Bloger"],
-    typeSpeed:80,
-    backSpeed:80,
-    backdelay:1000,
-    loop:true
-});
+// let typed = new typed(".multi-headline",{
+//     strings:["Frontend Developer",  "MERN Developer","Graphic Designer","Freelancer","Bloger"],
+//     typeSpeed:80,
+//     backSpeed:80,
+//     backdelay:1000,
+//     loop:true
+// });
+
+let headingChange = {
+    heading: function () {
+        var headings = ['Hello', 'Hi', 'Ye'];
+        for (var i = 0; i < headings.length; i++) {
+            document.getElementById('heading').innerHTML = headings[i];
+        }
+    }
+};   
