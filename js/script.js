1b9a513ea4037152126dddfb08e12ae4a73c700b
KARAN KUMAR <kkvmalhi@gmail.com>
2023-03-18 01:40:03 +0500

    working on responsiveness

--- js/script.js
+++ js/script.js
@@ -1,23 +1,7 @@
-let typed = new Typed(".multi-headline",{
+let typed = new typed(".multi-headline",{
     strings:["Frontend Developer",  "MERN Developer","Graphic Designer","Freelancer","Bloger"],
     typeSpeed:80,
     backSpeed:80,
     backdelay:1000,
     loop:true
-})
-
-
-// // change bg color on scroll
-
-
-// let navbar = document.querySelector('header')
-
-// window.onscroll = function() {
-
-// // pageYOffset or scrollY
-// if (window.pageYOffset > 0) {
-// navbar.classList.add('scrolled')
-// } else {
-// navbar.classList.remove('scrolled')
-// }
-// }
+});
