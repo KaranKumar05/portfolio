3b938e22915474bf101d39520f0b741e897bc047
KARAN KUMAR <kkvmalhi@gmail.com>
2023-03-20 23:38:12 +0500

    pending

--- js/script.js
+++ js/script.js
@@ -1,16 +1,10 @@
-// let typed = new typed(".multi-headline",{
-//     strings:["Frontend Developer",  "MERN Developer","Graphic Designer","Freelancer","Bloger"],
-//     typeSpeed:80,
-//     backSpeed:80,
-//     backdelay:1000,
-//     loop:true
-// });
 
-let headingChange = {
-    heading: function () {
-        var headings = ['Hello', 'Hi', 'Ye'];
-        for (var i = 0; i < headings.length; i++) {
-            document.getElementById('heading').innerHTML = headings[i];
-        }
-    }
-};   
+// Let = multi-headline = document.getElementById("multi-headline");
+let multi_headline = document.getElementById("multi-headline");
+let typed = new Typed(".multi_headline", {
+    strings: ["Frontend Developer", "MERN Developer", "Graphic Designer", "Freelancer", "Bloger"],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backdelay: 1000,
+    loop: true
+});
