1f95e58d145009b521761d8c91501d73de2f09ba
KARAN KUMAR <kkvmalhi@gmail.com>
2023-03-23 21:33:23 +0500

    working on responsive

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 // Let = multi-headline = document.getElementById("multi-headline");
-let multi_headline = document.getElementById("multi-headline");
+let multi_headline = document.getElementById("multi_headline");
 let typed = new Typed(".multi_headline", {
     strings: ["Frontend Developer", "MERN Developer", "Graphic Designer", "Freelancer", "Bloger"],
     typeSpeed: 80,
