00504e3d8e3b1f31a7b1344652859f60b7529aa8
Karan Kumar <125303637+KaranKumar05@users.noreply.github.com>
2023-03-24 22:30:36 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
-let typed = new Typed(".multi-headline", {
-    strings: ["Frontend Developer", "MERN Developer", "Graphic Designer", "Freelancer", "Bloger"],
-    typeSpeed: 80,
-    backSpeed: 80,
-    backdelay: 1000,
-    loop: true
-});
+let typed = new Typed(".multi-headline",{
+    strings:["Frontend Developer",  "MERN Developer","Graphic Designer","Freelancer","Bloger"],
+    typeSpeed:80,
+    backSpeed:80,
+    backdelay:1000,
+    loop:true
+})
