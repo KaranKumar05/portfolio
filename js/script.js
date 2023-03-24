7adbcb2d8088006fc26f2981f423666814f0ba66
Karan Kumar <125303637+KaranKumar05@users.noreply.github.com>
2023-03-24 22:20:21 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 
-// Let = multi-headline = document.getElementById("multi-headline");
-let multi_headline = document.getElementById("multi_headline");
-let typed = new Typed(".multi_headline", {
+let typed = new Typed(".multi-headline", {
     strings: ["Frontend Developer", "MERN Developer", "Graphic Designer", "Freelancer", "Bloger"],
     typeSpeed: 80,
     backSpeed: 80,
